5f26221f2e4ce6d0afd793a16a5f980c5b788fbd
Luan Braz <luan_silvacpv@hotmail.com>
2023-07-15 14:14:31 -0300

    Start projeto

---
+++ gerenciamento-cafe-api/src/main/java/com/luan/gerenciamentocafeapi/GerenciamentoCafeApiApplication.java
@@ -0,0 +1,13 @@
+package com.luan.gerenciamentocafeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCafeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCafeApiApplication.class, args);
+	}
+
+}
---
+++ gerenciamento-cafe-api/src/test/java/com/luan/gerenciamentocafeapi/GerenciamentoCafeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan.gerenciamentocafeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCafeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
