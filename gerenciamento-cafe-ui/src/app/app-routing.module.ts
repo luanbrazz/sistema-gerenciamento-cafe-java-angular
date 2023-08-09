9311759b31bda574c4b5e429e47c33c441984763
Luan Braz <luan_silvacpv@hotmail.com>
2023-08-09 17:33:22 -0300

    Configura e documenta rotas principais no AppRoutingModule
    - Define rota padrão para o HomeComponent.
    - Adiciona rota "cafe" que redireciona para o dashboard e possui rotas filhas lazy-loaded para componentes e dashboard.
    - Implementa rota coringa para redirecionar caminhos não definidos para o HomeComponent.
    - Adiciona comentários detalhados explicando cada seção da configuração de rota.

--- gerenciamento-cafe-ui/src/app/app-routing.module.ts
+++ gerenciamento-cafe-ui/src/app/app-routing.module.ts
@@ -1,35 +1,57 @@
+// Importa o NgModule, que é usado para criar um módulo Angular.
 import { NgModule } from '@angular/core';
+
+// Importa os recursos necessários do módulo de roteamento do Angular.
 import { RouterModule, Routes } from '@angular/router';
+
+// Importa o componente HomeComponent.
 import { HomeComponent } from './home/home.component';
+
+// Importa o componente FullComponent.
 import { FullComponent } from './layouts/full/full.component';
 
+// Define uma constante chamada "routes" que contém a configuração das rotas da aplicação.
 const routes: Routes = [
+  // Rota raiz que direciona para o componente HomeComponent.
   { path: '', component: HomeComponent },
+
+  // Rota que começa com "cafe" e utiliza o componente FullComponent.
   {
     path: 'cafe',
     component: FullComponent,
     children: [
+      // Quando a rota é exatamente "cafe", redireciona para "/cafe/dashboard".
       {
         path: '',
         redirectTo: '/cafe/dashboard',
         pathMatch: 'full',
       },
+
+      // Rota lazy-loaded que carrega o módulo MaterialComponentsModule quando acessada.
       {
         path: '',
         loadChildren:
           () => import('./material-component/material.module').then(m => m.MaterialComponentsModule),
       },
+
+      // Rota lazy-loaded que carrega o módulo DashboardModule quando "cafe/dashboard" é acessada.
       {
         path: 'dashboard',
         loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
       }
     ]
   },
+
+  // Rota coringa que pega todos os caminhos não definidos e redireciona para o componente HomeComponent.
   { path: '**', component: HomeComponent }
 ];
 
+// Define um novo módulo chamado AppRoutingModule.
 @NgModule({
+  // Configura o RouterModule para usar as rotas definidas anteriormente.
   imports: [RouterModule.forRoot(routes)],
+
+  // Exporta o RouterModule para que outros módulos possam usar as rotas definidas.
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
